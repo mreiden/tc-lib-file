87596f60d5eef4c53b94aa38215d4bc2acf29eac
Mark Reidenbach <mark@digitalmanagementteam.com>
2026-06-15 10:42:18 -0500

    File: Compare url scheme case-insensitively (Allow FILE://) and add test

--- test/FileTest.php
+++ test/FileTest.php
@@ -1065,6 +1065,10 @@ class FileTest extends TestUtil
 
         $fileUrl = 'file://some/file.txt';
         $this->assertTrue($file->isValidFile($fileUrl));
+
+        // URL schemes are case-insensitive per RFC 3986
+        $fileUrl = 'FILE://some/file.txt';
+        $this->assertTrue($file->isValidFile($fileUrl));
     }
 
     #[Test]
